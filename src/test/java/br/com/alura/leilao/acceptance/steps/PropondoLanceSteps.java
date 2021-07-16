c0d8b7b9cc12bd8d8a99a7bc3599cfff145043b1
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-07-15 21:17:10 -0300

    Implementando primeiro teste com Cucumber, validando unico lance valido

---
+++ src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
@@ -0,0 +1,35 @@
+package br.com.alura.leilao.acceptance.steps;
+
+import br.com.alura.leilao.model.Lance;
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Usuario;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import java.math.BigDecimal;
+
+public class PropondoLanceSteps {
+
+    private Lance lance;
+    private Leilao leilao;
+
+    @Given("dado um lance valido")
+    public void dado_um_lance_valido() {
+        Usuario usuario = new Usuario("fulano");
+        lance = new Lance(usuario, BigDecimal.TEN);
+    }
+
+    @When("Quando propoe o lance")
+    public void quando_propoe_o_lance() {
+        leilao = new Leilao("Tablet Tal");
+        leilao.propoe(lance);
+    }
+
+    @Then("Entao o lance eh aceito")
+    public void entao_o_lance_eh_aceito() {
+        Assert.assertEquals(1, leilao.getLances().size());
+        Assert.assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
+    }
+}
--- src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -4,9 +4,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.math.BigDecimal;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class LeilaoTest {
+
+    @Nested
+    @DisplayName("Dado um lance valido")
+    class UnicoLanceTests {
+
+        Leilao leilao = new Leilao("Macbook Pro 15");
+        BigDecimal doisMil = new BigDecimal("2000.0");
+
+        @Nested
+        @DisplayName("Quando propoe o lance")
+        class AssertLanceNormal {
+
+            @Test
+            @DisplayName("Entao o lance eh aceito")
+            public void deveReceberUmLance() {
+
+                leilao.propoe(new Lance(new Usuario("Steve Jobs"), doisMil));
+
+                assertEquals(1, leilao.getLances().size());
+                assertEquals(doisMil, leilao.getLances().get(0).getValor());
+            }
+        }
+    }
 	
 	@Test
     public void deveReceberUmLance() {
